6f2563ff5e26fba44fa5e87f15badad35bfc3a75
pangilinan-patrick <97028950+pangilinan-patrick@users.noreply.github.com>
2023-04-02 09:13:59 +0800

    move buttons to a component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "./App.scss";
+import Buttons from "./components/Buttons";
 
 function App() {
   return (
     <div className="App">
       <div className="calc">
         <div className="panel"></div>
-        <div className="buttons-container">
-          <div className="buttons">7</div>
-          <div className="buttons">8</div>
-          <div className="buttons">9</div>
-          <div className="buttons">4</div>
-          <div className="buttons">5</div>
-          <div className="buttons">6</div>
-          <div className="buttons">1</div>
-          <div className="buttons">2</div>
-          <div className="buttons">3</div>
-        </div>
+        <Buttons />
       </div>
     </div>
   );
---
+++ src/components/Buttons.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Buttons extends Component {
+  render() {
+    return (
+      <div className="buttons-container">
+        <div className="buttons">7</div>
+        <div className="buttons">8</div>
+        <div className="buttons">9</div>
+        <div className="buttons">4</div>
+        <div className="buttons">5</div>
+        <div className="buttons">6</div>
+        <div className="buttons">1</div>
+        <div className="buttons">2</div>
+        <div className="buttons">3</div>
+        <div className="buttons">0</div>
+        <div className="buttons">00</div>
+        <div className="buttons">.</div>
+      </div>
+    );
+  }
+}
+
+export default Buttons;
